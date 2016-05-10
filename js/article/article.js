1ee5f80872f2d15a4a97ea3135115922f5468dc6
CharlesMoone <meng5994197@gmail.com>
2016-05-10 21:14:06 +0800

    代码重构
    bug很严重无法修复

--- js/article/article.js
+++ js/article/article.js
@@ -6,82 +6,32 @@ window.onload = function () {
     preload.children[0].className = "finish";
     preload.remove();
 
-    var length = {start: 0, end: 0, canRefresh: true, canNode: true};
+    var length = {start: 0, end: 0, canRefresh: true};
     eventListener(length);
 };
 
 var eventListener = function (length) {
 
     var article = document.getElementsByTagName("article")[0];
+    var loading = document.getElementsByClassName("loading")[0];
 
     document.addEventListener('touchstart', function (e) {
-        if (!length.canRefresh) {
-            return ;
+        if (length.canRefresh) {
+            length.start = e.targetTouches[0].pageY;
         }
-        length.start = length.end = e.targetTouches[0].pageY;
-
     });
 
     document.addEventListener('touchmove', function (e) {
-        length.end = e.targetTouches[0].pageY;
-        var realLength = length.end - length.start;
-        if (!length.canRefresh || realLength <= 0 || window.scrollY != 0) {
-            return ;
+        if (length.canRefresh) {
+            e.preventDefault();
+            length.end = e.targetTouches[0].pageY;
+            article.style.marginTop = (length.end - length.start) + "px";
         }
-        if (length.canNode && realLength > 5 && window.scrollY == 0) {
-            var loading = new Node({node: "DIV", class: "loading"}, document.getElementsByTagName("section")[0], "松开就加载给你看!");
-            length.canNode = false;
-        }
-        e.preventDefault();
-        article.style.marginTop = realLength + "px";
     });
 
     document.addEventListener('touchend', function (e) {
-        var realLength = length.end - length.start;
-        var loading = document.getElementsByClassName("loading")[0];
-        // if (!length.canRefresh || realLength <= 0) {
-        //     if (loading != undefined) loading.remove();
-        //     article.style.marginTop = null;
-        // } else {
-        //     if (realLength > 50) {
-        //         loading.innerHTML = "好吧,那就加载给你看!";
-        //         length.end = length.start + 50;
-        //         article.style.marginTop = (length.end - length.start) + "px";
-        //         length.canRefresh = false;
-        //         refresh(length, article, loading);
-        //     } else if ((length.end - length.start) > 0) {
-        //         if (loading != undefined) loading.remove();
-        //         article.style.marginTop = null;
-        //     }
-        // }
-
-        if (length.canRefresh && realLength > 50 && window.scrollY == 0 && loading != undefined) {
-            loading.innerHTML = "好吧,那就加载给你看!";
-            // length.end = length.start + 50;
-            article.style.marginTop = "50px";
+        if (length.canRefresh) {
             length.canRefresh = false;
-            refresh(length, article, loading);
-            return ;
         }
-        if (loading != undefined) loading.remove();
-        article.style.marginTop = null;
     });
-};
-
-var refresh = function (length, target, loading) {
-    setTimeout(function () {
-        loading.remove();
-        var article = document.getElementById("article");
-        target.style.marginTop = null;
-        length.canRefresh = true;
-        length.canNode = true;
-
-        var i, section, h4, p, button;
-        for (i = 0; i < 4; i ++) {
-            section = new Node({node: "SECTION", before: true, beforeNumber: 1}, article);
-            h4 = new Node({node: "H4"}, section.n, "this is title " + i);
-            p = new Node({node: "P"}, section.n, "this is a description about the title.");
-            button = new Node({node: "BUTTON", class: "share"}, section.n);
-        }
-    }, 3000);
 };
